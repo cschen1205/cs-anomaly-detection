170a178640daf913a56de0a6d423401d6601edd4
xschen <xs0040@gmail.com>
2018-04-02 11:06:05 +0800

    source upload

---
+++ project/SimuKit.ML.AnomalyDetection.UT/MultiVariateGaussianDistributionAD_UT.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using SimuKit.ML.Lang;
+using SimuKit.ML.Util;
+
+namespace SimuKit.ML.AnomalyDetection.UT
+{
+    public class MultiVariateGaussianDistributionAD_UT
+    {
+        
+
+        public static void Test_LoadDataSet()
+        {
+            List<MLDataPoint> X1 = MLDataPointUtil.LoadDataSet("X1.txt");
+            List<MLDataPoint> Xval1 = MLDataPointUtil.LoadDataSet("Xval1.txt");
+            List<MLDataPoint> yval1 = MLDataPointUtil.LoadDataSet("yval1.txt");
+
+            List<MLDataPoint> X2 = MLDataPointUtil.LoadDataSet("X2.txt");
+            List<MLDataPoint> Xval2 = MLDataPointUtil.LoadDataSet("Xval2.txt");
+            List<MLDataPoint> yval2 = MLDataPointUtil.LoadDataSet("yval2.txt");
+
+            int row_count, col_count;
+
+            MLDataPointUtil.GetSize(X1, out row_count, out col_count);
+            Console.WriteLine("size(X1)=[{0} {1}]", row_count, col_count);
+
+            MLDataPointUtil.GetSize(Xval1, out row_count, out col_count);
+            Console.WriteLine("size(Xval1)=[{0} {1}]", row_count, col_count);
+
+            MLDataPointUtil.GetSize(yval1, out row_count, out col_count);
+            Console.WriteLine("size(yval1)=[{0} {1}]", row_count, col_count);
+
+            MLDataPointUtil.GetSize(X2, out row_count, out col_count);
+            Console.WriteLine("size(X2)=[{0} {1}]", row_count, col_count);
+
+            MLDataPointUtil.GetSize(Xval2, out row_count, out col_count);
+            Console.WriteLine("size(Xval2)=[{0} {1}]", row_count, col_count);
+
+            MLDataPointUtil.GetSize(yval2, out row_count, out col_count);
+            Console.WriteLine("size(yval2)=[{0} {1}]", row_count, col_count);
+        }
+
+        
+
+        /// <summary>
+        /// test probability calculation
+        /// </summary>
+        public static void Test_CalcProbability_X(int data_set_index)
+        {
+            List<MLDataPoint> X = MLDataPointUtil.LoadDataSet(string.Format("X{0}.txt", data_set_index));
+
+            MultiVariateGaussianDistributionAD<MLDataPoint> algorithm = new MultiVariateGaussianDistributionAD<MLDataPoint>();
+            algorithm.ComputeGaussianDistribution(X);
+
+            List<MLDataPoint> correct_p = MLDataPointUtil.LoadDataSet(string.Format("p{0}.txt", data_set_index));
+
+            int row_count = X.Count;
+
+            double total_error = 0;
+            for (int i = 0; i < row_count; ++i)
+            {
+                double p = algorithm.CalcProbability(X[i]);
+                double error=System.Math.Abs(correct_p[i][0] - p);
+                total_error += error;
+                Console.WriteLine("p={0} correct_p={1}", p, correct_p[i]);
+            }
+
+            Console.WriteLine("Total error: {0}", total_error);
+
+        }
+
+        /// <summary>
+        /// Test cross validation 
+        /// </summary>
+        public static void Test_CalcProbability_Xval(int data_set_index)
+        {
+            List<MLDataPoint> X = MLDataPointUtil.LoadDataSet(string.Format("X{0}.txt", data_set_index));
+            List<MLDataPoint> Xval = MLDataPointUtil.LoadDataSet(string.Format("Xval{0}.txt", data_set_index));
+
+            MultiVariateGaussianDistributionAD<MLDataPoint> algorithm = new MultiVariateGaussianDistributionAD<MLDataPoint>();
+            algorithm.ComputeGaussianDistribution(X);
+
+            List<MLDataPoint> correct_pval = MLDataPointUtil.LoadDataSet(string.Format("pval{0}.txt", data_set_index));
+
+            int row_count = Xval.Count;
+
+            double total_error = 0;
+            for (int i = 0; i < row_count; ++i)
+            {
+                double pval = algorithm.CalcProbability(Xval[i]);
+                double error = System.Math.Abs(correct_pval[i][0] - pval);
+                total_error += error;
+                Console.WriteLine("pval={0} correct_pval={1}", pval, correct_pval[i]);
+            }
+
+            Console.WriteLine("Total error: {0}", total_error);
+        }
+
+        protected static void SelectThreshold(int data_set_index, out double F1Score, out double threshold)
+        {
+            List<MLDataPoint> X = MLDataPointUtil.LoadDataSet(string.Format("X{0}.txt", data_set_index));
+            List<MLDataPoint> Xval = MLDataPointUtil.LoadDataSet(string.Format("Xval{0}.txt", data_set_index));
+            List<MLDataPoint> yval_temp = MLDataPointUtil.LoadDataSet(string.Format("yval{0}.txt", data_set_index));
+
+            bool[] yval = new bool[yval_temp.Count];
+
+            int row_count = yval_temp.Count;
+            for (int i = 0; i < row_count; ++i)
+            {
+                yval[i] = yval_temp[i][0] > 0.5;
+            }
+
+            MultiVariateGaussianDistributionAD<MLDataPoint> algorithm = new MultiVariateGaussianDistributionAD<MLDataPoint>();
+            
+            threshold = algorithm.SelectThreshold(X, Xval, yval, out F1Score);
+        }
+
+        public static void Test_SelectThreshold(int data_set_index)
+        {
+            double threshold, F1Score;
+            SelectThreshold(data_set_index, out F1Score, out threshold);
+
+            Console.WriteLine("Threshold: {0}", threshold);
+            Console.WriteLine("F1Score: {0}", F1Score);
+
+        }
+
+        public static void Test_FindOutliers(int data_set_index)
+        {
+            double threshold, F1Score;
+            SelectThreshold(data_set_index, out F1Score, out threshold);
+
+            List<MLDataPoint> X = MLDataPointUtil.LoadDataSet(string.Format("X{0}.txt", data_set_index));
+            List<MLDataPoint> correct_outliers = MLDataPointUtil.LoadDataSet(string.Format("outliers{0}.txt", data_set_index));
+
+            Console.WriteLine("Correct Outliers:");
+            for (int i = 0; i < correct_outliers.Count; ++i)
+            {
+                Console.WriteLine("{0}", correct_outliers[i][0]);
+            }
+
+            MultiVariateGaussianDistributionAD<MLDataPoint> algorithm = new MultiVariateGaussianDistributionAD<MLDataPoint>();
+            algorithm.ComputeGaussianDistribution(X);
+            List<int> outliers=algorithm.FindOutliers(X, threshold);
+
+            Console.WriteLine("Predict Outliers:");
+            for (int i = 0; i < outliers.Count; ++i)
+            {
+                Console.WriteLine("{0}", outliers[i]+1);
+            }
+
+            Console.WriteLine("Point Count: {0}", X.Count);
+
+            Console.WriteLine("Threshold: {0}", threshold);
+
+            Console.WriteLine("Predict Outliers Count; {0} Correct Outliers Count: {1}", outliers.Count, correct_outliers.Count);
+        }
+
+    }
+}
---
+++ project/SimuKit.ML.AnomalyDetection/GaussianDistributionAD.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimuKit.ML.Lang;
+
+namespace SimuKit.ML.AnomalyDetection
+{
+    /// <summary>
+    /// Unsupervised anomaly detection algorithm based on Gaussian distribution
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GaussianDistributionAD<T>
+        where T : MLDataPoint
+    {
+        private List<int> mFeatureIndexList = new List<int>();
+        private HashSet<int> mFeatureIndexSet = new HashSet<int>();
+        private Dictionary<int, double> mGaussian_Mu = new Dictionary<int, double>();
+        private Dictionary<int, double> mGaussian_Sigma2 = new Dictionary<int, double>();
+        private double mEpsilon = 0.02;
+
+        public Dictionary<int, double> Gaussian_Mu
+        {
+            get { return mGaussian_Mu; }
+        }
+
+        public Dictionary<int, double> Gaussian_Sigma2
+        {
+            get { return mGaussian_Sigma2; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(int feature_index in mFeatureIndexList)
+            {
+                sb.AppendFormat("mu[{0}]={1:0.00}\n", feature_index, mGaussian_Mu[feature_index]);
+            }
+
+            foreach (int feature_index in mFeatureIndexList)
+            {
+                sb.AppendFormat("sigma^2[{0}]={1:0.000}\n", feature_index, mGaussian_Sigma2[feature_index]);
+            }
+
+            return sb.ToString();
+        }
+
+        public double Epsilon
+        {
+            get { return mEpsilon; }
+            set { mEpsilon = value; }
+        }
+
+        public void SelectFeature(int feature_index)
+        {
+            if (mFeatureIndexSet.Contains(feature_index))
+            {
+                return;
+            }
+            mFeatureIndexList.Add(feature_index);
+            mFeatureIndexSet.Add(feature_index);
+        }
+
+        public void ClearSelectedFeatures()
+        {
+            mFeatureIndexList.Clear();
+            mFeatureIndexSet.Clear();
+        }
+
+        public double FindProbability(int feature_index, double x)
+        {
+            double mu=mGaussian_Mu[feature_index];
+            double sigma2=mGaussian_Sigma2[feature_index];
+            return System.Math.Exp(-System.Math.Pow(x-mu, 2)/(2*sigma2)) / (System.Math.Sqrt(2*System.Math.PI*sigma2));
+        }
+
+        public double CalcProbability(T rec)
+        {
+            double product = 1;
+            foreach (int feature_index in mFeatureIndexList)
+            {
+                product*=FindProbability(feature_index, rec[feature_index]);
+            }
+            return product;
+        }
+
+        public bool IsAnomaly(T rec)
+        {
+            double p = CalcProbability(rec);
+            return p < mEpsilon;
+        }
+
+        public void ComputeGaussianDistribution(List<T> records)
+        {
+            int sample_count = records.Count;
+
+            if (mFeatureIndexList.Count == 0)
+            {
+                int dim = records[0].Dimension;
+                for (int i = 0; i < dim; ++i)
+                {
+                    SelectFeature(i);
+                }
+            }
+            
+            foreach (int feature_index in mFeatureIndexList)
+            {
+                double mu = 0;
+                for (int i = 0; i < sample_count; ++i)
+                {
+                    double x = records[i][feature_index];
+                    mu += x;
+                }
+                mu /= sample_count;
+
+                double sqr_sum = 0;
+                for (int i = 0; i < sample_count; ++i)
+                {
+                    double x = records[i][feature_index];
+                    sqr_sum += System.Math.Pow(x - mu, 2);
+                }
+
+                double sigma2 = sqr_sum / sample_count;
+
+                mGaussian_Mu[feature_index] = mu;
+                mGaussian_Sigma2[feature_index] = sigma2;
+            }
+            
+        }
+    }
+}
